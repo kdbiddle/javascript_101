2bb90c1b69e1069de319440855226e7cfe5f6915
Kevin Biddle <kdbiddle08@gmail.com>
2015-12-05 23:27:33 -0500

    Completed Sum of Even Fibonacci Numbers problem

--- Problems.js
+++ Problems.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-
+	//returns sum of all multiples of 3 and 5 up to the input
 	$("#getSolution1").click(function() {
 		var n = parseInt(document.getElementById("inputText").value);
 		var total = 0;
@@ -10,4 +10,21 @@ $(document).ready(function(){
 		};
   		$("#solution1").html(total);
   });
+
+		$("#getSolution2").click(function() {
+			var n = parseInt(document.getElementById("inputText2").value);
+			var total = 0;
+			var fibonacciSequence =[1,2];
+			for (var i=1; fibonacciSequence[i]<n; i++) {
+				var val = fibonacciSequence[i] + fibonacciSequence[i-1];
+				fibonacciSequence.push(val);
+			};
+			for (var i=0; i<fibonacciSequence.length; i++) {
+				if (fibonacciSequence[i] % 2 == 0) {
+					total += fibonacciSequence[i];
+				}
+			}
+  		$("#solution2").html(total);
+  });
+
 });
